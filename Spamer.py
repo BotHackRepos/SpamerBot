95fd5c338051668a804d60f649c19847d7d37c7d
bothack <100835053+BotHackRepos@users.noreply.github.com>
2022-05-13 13:01:14 +0300

    Update Spamer.py

--- Spamer.py
+++ Spamer.py
@@ -3,24 +3,16 @@ import telebot
 token = 'токен'
 bot = telebot.TeleBot(token)
 
-chat_id = '5077197358'
-
 
 @bot.message_handler(commands=["start"])
 def main(message):
      while True:
-           if (message.chat.id == chat_id):
-              print('No! This is developer bot')
-           else:
              print('Spam attack started')
              bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
 
 @bot.message_handler(commands=["1"])
 def foo(message):
      while True:
-           if (message.chat.id == chat_id):
-              print('No! This is developer bot')
-           else:
              print('Spam attack started')
              bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
 
@@ -28,9 +20,6 @@ def foo(message):
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
         while True:
-           if (message.chat.id == chat_id):
-              print('No! This is developer bot')
-           else:
              print('Spam attack started')
              bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
  
