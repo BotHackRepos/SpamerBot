126be3b8585bb5ea5082e934be9f885e7ed288f6
bothack <100835053+BotHackRepos@users.noreply.github.com>
2022-05-13 13:00:32 +0300

    Update Spamer.py

--- Spamer.py
+++ Spamer.py
@@ -3,24 +3,35 @@ import telebot
 token = 'токен'
 bot = telebot.TeleBot(token)
 
+chat_id = '5077197358'
+
 
 @bot.message_handler(commands=["start"])
 def main(message):
      while True:
-        print('Spam attack started')
-        bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
+           if (message.chat.id == chat_id):
+              print('No! This is developer bot')
+           else:
+             print('Spam attack started')
+             bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
 
 @bot.message_handler(commands=["1"])
 def foo(message):
      while True:
-        print('Spam attack started')
-        bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
+           if (message.chat.id == chat_id):
+              print('No! This is developer bot')
+           else:
+             print('Spam attack started')
+             bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
         while True:
-           print('Spam attack started')
-           bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
+           if (message.chat.id == chat_id):
+              print('No! This is developer bot')
+           else:
+             print('Spam attack started')
+             bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
  
 bot.polling()
