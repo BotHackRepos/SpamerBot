2d28665ec0298e126f10d80aa693dcb4976503ad
bothack <100835053+BotHackRepos@users.noreply.github.com>
2022-05-13 12:27:49 +0300

    Update Spamer.py

--- Spamer.py
+++ Spamer.py
@@ -7,17 +7,20 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=["start"])
 def main(message):
      while True:
+        print('Spam attack started')
         bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
 
 @bot.message_handler(commands=["1"])
 def foo(message):
      while True:
+        print('Spam attack started')
         bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
         while True:
+           print('Spam attack started')
            bot.send_message(message.chat.id, text='[][][][][[][][[][][]][[][][][][]]][[][][][][][]')
  
 bot.polling()
